37a7ac87b1929979630a30d090961ffccde84c0e
huseyin.gokdemir <huseyin.gokdemir@agarthateknoloji.com.tr>
2022-11-28 23:01:13 +0300

    Add JobController

---
+++ JobApplication/Controllers/JobController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobApplication.Controllers
+{
+    public class JobController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult JobDetails(int id)
+        {
+
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
